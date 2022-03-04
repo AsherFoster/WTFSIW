6c6296b2a942301c2f48452112ff81321a31a149
Asher Foster <asher@sharesies.co.nz>
2022-03-04 22:47:48 +1300

    Workaround, use old gcloud sdk

--- install_gcloud.sh
+++ install_gcloud.sh
@@ -1,9 +1,8 @@
 gcloud version || true
-if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then
-  rm -rf $HOME/google-cloud-sdk; export CLOUDSDK_CORE_DISABLE_PROMPTS=1
-  curl https://sdk.cloud.google.com | bash
+if [ ! -d "$HOME/google-cloud-sdk" ]; then
+  export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)";
+  echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list;
+  curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - ;
+  sudo apt-get update && sudo apt-get install google-cloud-sdk=212.0.0-0;
 fi
-# Add gcloud to $PATH
-source /home/travis/google-cloud-sdk/path.bash.inc
 gcloud version
-
